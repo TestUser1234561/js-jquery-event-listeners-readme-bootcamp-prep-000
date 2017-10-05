8b857fb7e766be3c736717b6095f1cfc874583fb
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 07:34:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 function pressIt() {
   $('#typing').on("keydown", function(k) {
     console.log(k)
-    if(k.key == 'G') {
+    if(k.which == 70) {
       alert("G")
     }
   })
